96d3f53c6f3efc713f80cc4bd8f234b442c20b88
Egor Mamtsev <egormamcev@MacBook-Air.local>
2024-12-29 14:14:04 -0600

    2nd try

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -22,19 +22,13 @@ class Herbivore extends Animal {
 }
 
 class Carnivore extends Animal {
-  constructor(name, health) {
-    super();
-  }
-
   bite(animal) {
-    if (animal.hidden === false) {
+    if (animal instanceof Herbivore && !animal.hidden) {
       animal.health -= 50;
     }
 
     if (animal.health <= 0) {
-      const index = Animal.alive.indexOf(animal);
-
-      Animal.alive.splice(index, 1);
+      Animal.alive = Animal.alive.filter((item) => item !== animal);
     }
   }
 }
